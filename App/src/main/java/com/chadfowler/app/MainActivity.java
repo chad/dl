55f9191077ba47cd2dcb256bdec0ed6e7f43a73e
Chad Fowler <chad@chadfowler.com>
2013-12-21 14:48:32 +0100

    simple stupid login dialog. deleting every boilerplate i can.

--- App/src/main/java/com/chadfowler/app/MainActivity.java
+++ App/src/main/java/com/chadfowler/app/MainActivity.java
@@ -1,65 +1,79 @@
 package com.chadfowler.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends ActionBarActivity {
+    public TextView userLabel;
+    public TextView passwordLabel;
+    public Button button;
+    public EditText emailField;
+    public EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LinearLayout main = new LinearLayout(this);
+        main.setLayoutParams(new android.app.ActionBar.LayoutParams(android.app.ActionBar.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        main.setOrientation(LinearLayout.VERTICAL);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+
+        setupLoginForm(main);
+
+        setContentView(main);
     }
 
+    private void setupLoginForm(LinearLayout main) {
+        LinearLayout userSection = new LinearLayout(this);
+        userSection.setOrientation(LinearLayout.HORIZONTAL);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+
+        userLabel = new TextView(this);
+        userLabel.setText("Email:");
+        emailField = new EditText(this);
+        emailField.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+
+        userSection.addView(userLabel);
+        userSection.addView(emailField);
+
+        LinearLayout passwordSection = new LinearLayout(this);
+        userSection.setOrientation(LinearLayout.HORIZONTAL);
+        passwordLabel = new TextView(this);
+        passwordLabel.setText("Password:");
+        passwordField = new EditText(this);
+        passwordField.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        passwordSection.addView(passwordLabel);
+        passwordSection.addView(passwordField);
+
+        button = new Button(this);
+        button.setText("Login");
+        button.setOnClickListener(makeClick());
+        main.addView(userSection);
+        main.addView(passwordSection);
+        main.addView(button);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected View.OnClickListener makeClick() {
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new Builder(v.getContext());
+
+                builder.setMessage("email: " + emailField.getText() + ", password: " + passwordField.getText())
+                        .setTitle("a dialog");
+                builder.show();
+            }
+        };
+
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-
 }
