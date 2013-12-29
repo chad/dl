151bd3e9f433b278e8be44ae80b8a9f3d0247f2e
Chad Fowler <chad@chadfowler.com>
2013-12-29 08:29:41 +0100

    Extracted view-related reminder stuff into a separate class.

--- App/src/main/java/com/chadfowler/app/MainActivity.java
+++ App/src/main/java/com/chadfowler/app/MainActivity.java
@@ -1,20 +1,14 @@
 package com.chadfowler.app;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.chadfowler.api.TaskService;
-
-import java.util.Date;
+import com.chadfowler.app.views.ReminderView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -38,34 +32,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void renderForm() {
-        LinearLayout main = new LinearLayout(this);
-        main.setLayoutParams(new android.app.ActionBar.LayoutParams(android.app.ActionBar.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        main.setOrientation(LinearLayout.VERTICAL);
-
-        taskLabel = new TextView(this);
-        taskLabel.setText("Remind me about:");
-        taskField = new EditText(this);
-        button = new Button(this);
-        button.setText("Remind me");
-        button.setOnClickListener(makeClick());
-        main.addView(taskLabel);
-        main.addView(taskField);
-        main.addView(button);
-        setContentView(main);
+        setContentView(new ReminderView(this));
     }
 
-    private View.OnClickListener makeClick() {
-        final Activity caller = this;
 
-        return new View.OnClickListener() {
-            public void onClick(View v) {
-                SharedPreferences settings = getSharedPreferences("dl", MODE_PRIVATE);
-                int userId = settings.getInt("userId", 0);
-                //FIXME userId is required
-                new TaskService(caller).addWithReminder(userId, taskField.getText().toString(), new Date());
-            }
-        };
-    }
 
 
 }
---
+++ App/src/main/java/com/chadfowler/app/views/ReminderView.java
@@ -0,0 +1,56 @@
+package com.chadfowler.app.views;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.chadfowler.api.TaskService;
+
+import java.util.Date;
+
+public class ReminderView extends LinearLayout {
+    private final Activity container;
+    private TextView taskLabel;
+    private EditText taskField;
+    private Button button;
+
+    public ReminderView(Activity container) {
+        super(container);
+        this.container = container;
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        setLayoutParams(new android.app.ActionBar.LayoutParams(android.app.ActionBar.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        setOrientation(LinearLayout.VERTICAL);
+        taskLabel = new TextView(container);
+        taskLabel.setText("Remind me about:");
+        taskField = new EditText(container);
+        button = new Button(container);
+        button.setText("Remind me");
+        button.setOnClickListener(makeClick());
+        addView(taskLabel);
+        addView(taskField);
+        addView(button);
+    }
+
+    private View.OnClickListener makeClick() {
+        final Activity caller = container;
+
+        return new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences settings = container.getSharedPreferences("dl", Context.MODE_PRIVATE);
+                int userId = settings.getInt("userId", 0);
+                //FIXME userId is required
+                new TaskService(caller).addWithReminder(userId, taskField.getText().toString(), new Date());
+            }
+        };
+    }
+
+}
